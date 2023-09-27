658ce9059f779e2a1bc7e7cca56f3282d0a2410c
umeneses <umeneses@student.42sp.org.br>
2023-09-27 18:49:10 -0300

    improving main tester and txt file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 14:43:28 by umeneses          #+#    #+#             */
-/*   Updated: 2023/09/26 18:14:07 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/09/27 16:21:58 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,23 @@
 int	main(void)
 {
 	int		fd;
+	int		index;
 	char	*ptr;
 
-	fd = open("big_line_with_nl", O_RDONLY);
-	ptr = "/// Reading file 'big_line_with_nl' ///";
+	fd = open("Mussum.txt", O_RDONLY);
+	ptr = "/// Reading file 'Mussum.txt' ///";
 	printf("\033[0;36m");
 	printf("\n%s\n\n", ptr);
+	index = 0;
 	while (ptr)
 	{
+		printf("\033[0;36m");
+		printf("GNL line %i = ", index);
 		printf("\033[0;33m");
 		ptr = get_next_line(fd);
 		printf("%s", ptr);
 		free (ptr);
+		index++;
 	}
 	close (fd);
 	return (0);
